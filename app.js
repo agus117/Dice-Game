e80b087638ce1af4eb4cc9ff775440ce3107ed63
agus117 <agustin117@outlook.com>
2019-03-24 23:30:38 -0600

    added functionality

--- app.js
+++ app.js
@@ -33,26 +33,63 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     }
     else 
-    {
-        //Clear roundScore.
-        roundScore = 0;
-        document.getElementById('current-' + activePlayer).textContent = 0;
-        
+    {  
         //Change activePlayer
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        //Animate new activePlayer
-        animateCSS('#name-' + activePlayer, 'bounce');
-        
-        //Change activePlayer background-color.
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        //document.querySelector('.player-0-panel').classList.remove('active');
-        //document.querySelector('.player-1-panel').classList.add('active');
-        
-        //Rehide dice
-        diceDOM.style.display = 'none';
+        nextPlayer();
     }
 });
+
+//Hold button functionality
+document.querySelector('.btn-hold').addEventListener('click', function() {
+   
+    //Add roundScore to the players total score
+    scores[activePlayer] += roundScore;
+    
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //Check if player has won the game
+    if(scores[activePlayer] >= 10) 
+    {
+        //Clear current score
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        //Update UI
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');     
+    }
+    else
+    {
+        //change player
+        nextPlayer();
+    }
+});
+
+//Adds points to the respective player, updates the UI and changes to the next player.
+function nextPlayer() {
+    
+    //Clear roundScore.
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    
+    //Change activePlayer
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    //Animate new activePlayer
+    animateCSS('#name-' + activePlayer, 'bounce');
+    
+    //Change activePlayer background-color.
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+    
+    //Rehide dice
+    diceDOM.style.display = 'none';
+}
 
 //Function for the animation of elements.
 function animateCSS(element, animationName, callback) {
@@ -76,9 +113,6 @@ function animateCSS(element, animationName, callback) {
 //innerHTML will set or retrieve text. Can also set new HTML elements for the content of the selected element.
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 //Asign a CSS property and value to an element.
-
-
-
 
 
 
